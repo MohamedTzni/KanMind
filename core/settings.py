340d9ee6bf60a25389313dbdefb9bb35f259ac5b
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-01-29 15:33:06 +0100

    fix: dynamic ALLOWED_HOSTS configuration via env vars

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-nur-fuer
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
