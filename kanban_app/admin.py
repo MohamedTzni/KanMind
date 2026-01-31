6d3be30601a31a1e1e78a5862b13624f9f41d79d
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-01 00:53:17 +0100

    Register Board, Task, and Comment in Django admin

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Board, Task, Comment
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title']
+    filter_horizontal = ['members']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'board', 'status', 'priority', 'created_at']
+    list_filter = ['status', 'priority', 'created_at']
+    search_fields = ['title', 'description']
+    filter_horizontal = ['assigned_to']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['task', 'author', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['text']
