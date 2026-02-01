921fd33f73ca147764ce548ff7d681ce0265c750
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-01 03:49:52 +0100

    Add TaskViewSet with custom actions for assigned and reviewing tasks

--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -34,3 +34,49 @@ class BoardViewSet(viewsets.ModelViewSet):
         Set the owner to the current user when creating a board.
         """
         serializer.save(owner=self.request.user)
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for Task model.
+    Provides CRUD operations for tasks.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Filter tasks to show only tasks from boards where user is owner or member.
+        """
+        user = self.request.user
+        user_boards = Board.objects.filter(owner=user) | Board.objects.filter(members=user)
+        return Task.objects.filter(board__in=user_boards)
+
+    def perform_create(self, serializer):
+        """
+        Set the created_by to the current user when creating a task.
+        """
+        serializer.save(created_by=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def assigned_to_me(self, request):
+        """
+        Custom endpoint: GET /api/tasks/assigned-to-me/
+        Returns all tasks assigned to the current user.
+        """
+        user = request.user
+        tasks = Task.objects.filter(assigned_to=user)
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def reviewing(self, request):
+        """
+        Custom endpoint: GET /api/tasks/reviewing/
+        Returns all tasks with status 'reviewing'.
+        """
+        user = request.user
+        user_boards = Board.objects.filter(owner=user) | Board.objects.filter(members=user)
+        tasks = Task.objects.filter(board__in=user_boards, status='reviewing')
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
