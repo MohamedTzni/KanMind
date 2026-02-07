cd5eee7f6a334847554ed192bc63e894cc84cf2e
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-08 00:53:22 +0100

    Enhance admin interface with custom configurations

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,6 +1,57 @@
 from django.contrib import admin
-from .models import Board, Task, Comment
+from kanban_app.models import Board, Task, Comment
 
-admin.site.register(Board)
-admin.site.register(Task)
-admin.site.register(Comment)
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    """Admin configuration for Board model"""
+    list_display = ['title', 'owner', 'get_member_count', 'created_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title', 'owner__username', 'owner__email']
+    readonly_fields = ['created_at', 'updated_at']
+    filter_horizontal = ['members']
+    
+    def get_member_count(self, obj):
+        """Display member count"""
+        return obj.members.count()
+    get_member_count.short_description = 'Members'
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """Admin configuration for Task model"""
+    list_display = ['title', 'board', 'status', 'priority', 'created_by', 'created_at']
+    list_filter = ['status', 'priority', 'created_at', 'board']
+    search_fields = ['title', 'description', 'created_by__username']
+    readonly_fields = ['created_at', 'updated_at']
+    filter_horizontal = ['assigned_to']
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('board', 'title', 'description')
+        }),
+        ('Status & Priority', {
+            'fields': ('status', 'priority')
+        }),
+        ('Assignment', {
+            'fields': ('created_by', 'assigned_to')
+        }),
+        ('Dates', {
+            'fields': ('due_date', 'created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Admin configuration for Comment model"""
+    list_display = ['get_short_text', 'task', 'author', 'created_at']
+    list_filter = ['created_at', 'task__board']
+    search_fields = ['text', 'author__username', 'task__title']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    def get_short_text(self, obj):
+        """Display shortened comment text"""
+        return obj.text[:50] + '...' if len(obj.text) > 50 else obj.text
+    get_short_text.short_description = 'Comment'
