f5ce3951aa80dacf4f8d5233a66c94b9170bde71
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-07 15:16:29 +0100

    Add URLs for logout and profile endpoints

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from auth_app.api.views import RegistrationView, LoginView
+from auth_app.api.views import RegistrationView, LoginView, LogoutView, UserProfileView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
