1bd5cc9836eed38b23ff68bd6bdfa5fbe036f76c
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-09 19:19:53 +0100

    Add email-check endpoint to auth_app

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from kanban_app.api.views import BoardViewSet, TaskViewSet, CommentViewSet, UserViewSet
+from kanban_app.api.views import BoardViewSet, TaskViewSet, CommentViewSet, UserViewSet, AssignedToMeView, ReviewingTasksView
 
 router = DefaultRouter()
 router.register(r'boards', BoardViewSet, basename='board')
@@ -10,4 +10,6 @@ router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('tasks/assigned-to-me/', AssignedToMeView.as_view(), name='assigned-to-me'),
+    path('tasks/reviewing/', ReviewingTasksView.as_view(), name='reviewing-tasks'),
 ]
