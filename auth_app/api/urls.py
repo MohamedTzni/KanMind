1309d449797e4e7b682e789fa7bdb1f9df160281
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-09 19:09:18 +0100

    Add email-check endpoint to auth_app

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from auth_app.api.views import RegistrationView, LoginView, LogoutView, UserProfileView
+from auth_app.api.views import RegistrationView, LoginView, LogoutView, UserProfileView, EmailCheckView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('email-check/', EmailCheckView.as_view(), name='email-check'),
 ]
