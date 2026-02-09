c696c7bd2b377bdf63c7ef6ac581d7a564087b43
Mohamed Touzani <mohamed.touzani@gmx.de>
2026-02-09 20:54:43 +0100

    feat: Initialize backend with core Django REST Framework setup, authentication APIs, Kanban application structure, and corresponding tests.

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,10 +1,11 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.authtoken.models import Token
-
 from django.contrib.auth.models import User
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from auth_app.api.serializers import RegistrationSerializer, LoginSerializer
 
 
